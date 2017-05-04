f5e12f80a1ab90cfae10c1330bda6e5442d520a3
Wenkai Yin <yinw@vmware.com>
2017-05-04 19:12:16 +0800

    create different security context according to the rquest

--- src/ui/filter/security.go
+++ src/ui/filter/security.go
@@ -15,17 +15,25 @@
 package filter
 
 import (
-	"net/http"
 	"strings"
 
 	"github.com/astaxie/beego/context"
-	"github.com/vmware/harbor/src/common/security"
+	"github.com/vmware/harbor/src/common"
+	"github.com/vmware/harbor/src/common/dao"
+	"github.com/vmware/harbor/src/common/models"
+	"github.com/vmware/harbor/src/common/security/rbac"
+	"github.com/vmware/harbor/src/common/security/secret"
 	"github.com/vmware/harbor/src/common/utils/log"
+	"github.com/vmware/harbor/src/ui/auth"
+	"github.com/vmware/harbor/src/ui/config"
+	"github.com/vmware/harbor/src/ui/projectmanager"
 )
 
 const (
 	// HarborSecurityContext is the name of security context passed to handlers
 	HarborSecurityContext = "harbor_security_context"
+	// HarborProjectManager is the name of project manager passed to handlers
+	HarborProjectManager = "harbor_project_manager"
 )
 
 // SecurityFilter authenticates the request and passes a security context with it
@@ -40,20 +48,88 @@ func SecurityFilter(ctx *context.Context) {
 		return
 	}
 
-	if !strings.HasPrefix(req.RequestURI, "/api/") &&
-		!strings.HasPrefix(req.RequestURI, "/service/") {
+	if !strings.HasPrefix(req.URL.RequestURI(), "/api/") &&
+		!strings.HasPrefix(req.URL.RequestURI(), "/service/") {
 		return
 	}
 
-	securityCtx, err := createSecurityContext(req)
-	if err != nil {
-		log.Warningf("failed to create security context: %v", err)
-		return
-	}
-
-	ctx.Input.SetData(HarborSecurityContext, securityCtx)
+	// fill ctx with security context and project manager
+	fillContext(ctx)
 }
 
-func createSecurityContext(req *http.Request) (security.Context, error) {
-	return nil, nil
+func fillContext(ctx *context.Context) {
+	// secret
+	scrt := ctx.GetCookie("secret")
+	if len(scrt) != 0 {
+		ctx.Input.SetData(HarborProjectManager,
+			getProjectManager())
+
+		log.Info("creating a secret security context...")
+		ctx.Input.SetData(HarborSecurityContext,
+			secret.NewSecurityContext(scrt, config.SecretStore))
+
+		return
+	}
+
+	var user *models.User
+	var err error
+
+	// session
+	username := ctx.Input.Session("username")
+	if username != nil {
+		user, err = dao.GetUser(models.User{
+			Username: username.(string),
+		})
+		if err != nil {
+			log.Errorf("failed to get user %s: %v", username.(string), err)
+		}
+
+		if user != nil {
+			log.Info("get credential from session")
+		}
+	}
+
+	// basic auth
+	if user == nil {
+		username, password, ok := ctx.Request.BasicAuth()
+		if ok {
+			// TODO the return data contains other params when integrated
+			// with vic
+			user, err = auth.Login(models.AuthModel{
+				Principal: username,
+				Password:  password,
+			})
+			if err != nil {
+				log.Errorf("failed to authenticate %s: %v", username, err)
+			}
+			if user != nil {
+				log.Info("get credential from basic auth")
+			}
+		}
+	}
+
+	if user == nil {
+		log.Info("get no credential")
+	}
+
+	pm := getProjectManager()
+	ctx.Input.SetData(HarborProjectManager, pm)
+
+	log.Info("creating a rbac security context...")
+	ctx.Input.SetData(HarborSecurityContext,
+		rbac.NewSecurityContext(user, pm))
+
+	return
+}
+
+func getProjectManager(token ...string) projectmanager.ProjectManager {
+	if len(config.DeployMode()) == 0 ||
+		config.DeployMode() == common.DeployModeStandAlone {
+		log.Info("filling a project manager based on database...")
+		return config.DBProjectManager
+	}
+
+	// TODO create project manager based on pms
+	log.Info("filling a project manager based on pms...")
+	return nil
 }
