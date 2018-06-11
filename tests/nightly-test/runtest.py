0cf02de3b31290c33e963cd0f7dae70a119225c4
wangyan <wangyan@vmware.com>
2018-06-11 03:37:45 -0700

    fix issue

--- tests/nightly-test/runtest.py
+++ tests/nightly-test/runtest.py
@@ -13,27 +13,25 @@ import argparse
 class Parameters(object):
     def __init__(self):
         self.endpoint = ''
-        self.auth_mode = ''
         self.init_from_input()
 
     @staticmethod
     def parse_input():
         parser = argparse.ArgumentParser(description='run testcase') 
-        parser.add_argument('--auth-mode', '-a', dest='auth_mode', required=True, help='db, ldap or uaa.')
         parser.add_argument('--endpoint', '-e', dest='endpoint', required=True, help='The endpoint of harbor.')
 
         args = parser.parse_args()
-        return (args.endpoint, args.auth_mode)
+        return (args.endpoint)
 
     def init_from_input(self):
-        (self.endpoint, self.auth_mode) = Parameters.parse_input()
+        (self.endpoint) = Parameters.parse_input()
 
 
 def main():
     commandline_input = Parameters()
     
     try:
-        tc_executor = Executor(commandline_input.endpoint, commandline_input.auth_mode)
+        tc_executor = Executor(commandline_input.endpoint)
         tc_executor.execute()
 
     except Exception, e:
