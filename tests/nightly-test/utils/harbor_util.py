0ecfa529473daa160ff8c50ff529c47ffd48ae98
wangyan <wangyan@vmware.com>
2018-06-12 22:20:05 -0700

    update code

--- tests/nightly-test/utils/harbor_util.py
+++ tests/nightly-test/utils/harbor_util.py
@@ -58,3 +58,9 @@ def get_auth_mode(harbor_endpoint, harbor_user, harbor_pwd):
 def get_ca(harbor_endpoint, harbor_user, harbor_pwd):
    ca_contend = request(harbor_endpoint, '/systeminfo/getcert', 'get', harbor_user, harbor_pwd)
    open("/harbor/ca/ca.crt", 'wb').write(ca_contend)
+
+def main():
+    print get_ca('10.193.22.115', 'admin', 'Harbor12345')
+
+if __name__ == '__main__':
+    main()
