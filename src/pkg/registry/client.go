cb92829e66c4cf9e03805594f5440cddffbb8483
Wenkai Yin <yinw@vmware.com>
2020-04-15 11:22:18 +0800

    update

--- src/pkg/registry/client.go
+++ src/pkg/registry/client.go
@@ -403,7 +403,7 @@ func (c *client) monolithicBlobUpload(location, digest string, size int64, data 
 	if err != nil {
 		return err
 	}
-	req.ContentLength = size
+	req.ContentLength = -1
 	fmt.Printf("=======================before the do of mono blob upload \n")
 	resp, err := c.do(req)
 	if err != nil {
