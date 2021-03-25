7ddb16bd0d9a766e9d51e82574e53d2d7991f7d1
Wenkai Yin <yinw@vmware.com>
2021-03-25 16:37:20 +0800

    update
    Signed-off-by: Wenkai Yin <yinw@vmware.com>

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,25 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
-
-def warning(message) {
-    echo "WARNING: ${message}"
+def call(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
 }
